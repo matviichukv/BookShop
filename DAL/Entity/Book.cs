b50e760d75fefdce95086f33ccc541164c00b8f1
Besarab Vitaliy <besarabvitaliy@gmail.com>
2017-06-12 18:46:08 +0300

    Fixed DAL project

--- DAL/Entity/Book.cs
+++ DAL/Entity/Book.cs
@@ -6,22 +6,19 @@ using System.Threading.Tasks;
 
 namespace DAL.Entity
 {
-    class Book
+    public class Book
     {
-        public class Book
-        {
-            public int BookId { get; set; }
-            public string BookName { get; set; }
-            public DateTime DatePublished { get; set; }
-            public int PublisherId { get; set; }
-            public Publisher BookPublisher;
-            public int AuthorId { get; set; }
-            public Author BookAuthor { get; set; }
-            public int Volume { get; set; }
-            public string Language { get; set; }
-            public string Description { get; set; }
-            public int Price { get; set; }
-            public int Count { get; set; }
-        }
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public DateTime DatePublished { get; set; }
+        public int PublisherId { get; set; }
+        public Publisher BookPublisher;
+        public int AuthorId { get; set; }
+        public Author BookAuthor { get; set; }
+        public int Volume { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
     }
 }
